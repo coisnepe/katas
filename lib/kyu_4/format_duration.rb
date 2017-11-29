efdabf949e6fd8db44c6bd849cd6bbee4333c8bf
= <=>
2017-11-29 16:05:42 +0100

    WIP add comments

--- lib/kyu_4/format_duration.rb
+++ lib/kyu_4/format_duration.rb
@@ -1,6 +1,17 @@
 # url: https://www.codewars.com/kata/human-readable-duration-format/train/ruby
 
 module Kyu4
+  # hours: 1 * 3600
+  # minutes: 1 * 60
+  # seconds: 1 * 2
+  # 3662 / 3600 == 1 hour
+  # 3662 % 3600 == 62
+  # 62 / 60 == 1 minute
+  # 62 % 60 == 2 seconds
+  # 365 * 24 * 60 * 60
+  # year: 31_536_000
+  # day: 86_400
+  # hour: 60
   def self.format_duration(seconds)
     # Your code here
   end
--- spec/kyu_4/format_duration_spec.rb
+++ spec/kyu_4/format_duration_spec.rb
@@ -2,14 +2,19 @@
 
 describe Kyu4 do
   describe "#format_duration" do
-    it "makes it readable" do
+    it "makes minutes + seconds" do
       str = "1 minute and 2 seconds"
       expect(Kyu4.format_duration(62)).to eq(str)
     end
 
-    it "makes it readable" do
+    it "makes hour/minute/seconds readable" do
       str = "1 hour, 1 minute and 2 seconds"
       expect(Kyu4.format_duration(3662)).to eq(str)
+    end
+
+    it "makes days/hours/minutes readable" do
+      str = "2 days, 2 hours and 5 minutes"
+      expect(Kyu4.format_duration(180_300)).to eq(str)
     end
   end
 end
