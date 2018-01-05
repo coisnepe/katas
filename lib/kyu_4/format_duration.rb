329dc7da91e9ac24f2123e3d8da87c7c7c2493b6
= <=>
2018-01-05 11:08:06 +0100

    Functional kyu 4 format duration

--- lib/kyu_4/format_duration.rb
+++ lib/kyu_4/format_duration.rb
@@ -1,6 +1,14 @@
 # url: https://www.codewars.com/kata/human-readable-duration-format/train/ruby
 
 module Kyu4
+  EQUIVALENTS = {
+    year: 31_536_000.0,
+    day: 86_400.0,
+    hours: 3_600.0,
+    minute: 60.0,
+    second: 1
+  }.freeze
+
   # hours: 1 * 3600
   # minutes: 1 * 60
   # seconds: 1 * 2
@@ -9,10 +17,52 @@ module Kyu4
   # 62 / 60 == 1 minute
   # 62 % 60 == 2 seconds
   # 365 * 24 * 60 * 60
-  # year: 31_536_000
-  # day: 86_400
-  # hour: 60
   def self.format_duration(seconds)
-    # Your code here
+    return "now" if seconds.zero?
+    string_array = []
+
+    years = (seconds / 31_536_000.0).to_i
+    if years == 1
+      string_array << "1 year"
+    elsif years > 1
+      string_array << "#{years} years"
+    end
+    seconds -= 31_536_000 * years if years > 0
+
+    days = (seconds / 86_400.0).to_i
+    if days == 1
+      string_array << "1 day"
+    elsif days > 1
+      string_array << "#{days} days"
+    end
+    seconds -= 86_400 * days if days > 0
+
+    hours = (seconds / 3_600.0).to_i
+    if hours == 1
+      string_array << "1 hour"
+    elsif hours > 1
+      string_array << "#{hours} hours"
+    end
+    seconds -= 3_600 * hours if hours > 0
+
+    minutes = (seconds / 60.0).to_i
+    if minutes == 1
+      string_array << "1 minute"
+    elsif minutes > 1
+      string_array << "#{minutes} minutes"
+    end
+    seconds -= 60 * minutes if minutes > 0
+
+    if seconds == 1
+      string_array << "1 second"
+    elsif seconds > 1
+      string_array << "#{seconds} seconds"
+    end
+
+    if string_array.length == 1
+      string_array.first
+    else
+      string_array[0..-2].join(", ") + " and #{string_array[-1]}"
+    end
   end
 end
--- spec/kyu_4/format_duration_spec.rb
+++ spec/kyu_4/format_duration_spec.rb
@@ -1,20 +1,40 @@
-# url: https://www.codewars.com/kata/human-readable-duration-format/train/ruby
+# url: https://www.codewars.com/kata/human-duration-format/train/ruby
 
 describe Kyu4 do
   describe "#format_duration" do
-    it "makes minutes + seconds" do
+    it "processes 'now'" do
+      str = "now"
+      expect(Kyu4.format_duration(0)).to eq(str)
+    end
+
+    it "processes 1 second" do
+      str = "1 second"
+      expect(Kyu4.format_duration(1)).to eq(str)
+    end
+
+    it "processes multiple seconds" do
+      str = "2 seconds"
+      expect(Kyu4.format_duration(2)).to eq(str)
+    end
+
+    it "processes minutes + seconds" do
       str = "1 minute and 2 seconds"
       expect(Kyu4.format_duration(62)).to eq(str)
     end
 
-    it "makes hour/minute/seconds readable" do
+    it "processes hour/minute/seconds" do
       str = "1 hour, 1 minute and 2 seconds"
-      expect(Kyu4.format_duration(3662)).to eq(str)
+      expect(Kyu4.format_duration(3_662)).to eq(str)
     end
 
-    it "makes days/hours/minutes readable" do
+    it "processes days/hours/minutes" do
       str = "2 days, 2 hours and 5 minutes"
       expect(Kyu4.format_duration(180_300)).to eq(str)
+    end
+
+    it "processes year/seconds" do
+      str = "1 year and 2 seconds"
+      expect(Kyu4.format_duration(31_536_002)).to eq(str)
     end
   end
 end
